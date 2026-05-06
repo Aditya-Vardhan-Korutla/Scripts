7ad1cf9975c093060e685d074318196fc6b3c504
Aditya-Vardhan-Korutla <150161274+Aditya-Vardhan-Korutla@users.noreply.github.com>
2026-05-06 22:55:57 +0530

    script to install Tomcat 9 with OpenJDK 21

---
+++ tomcat-9-latest.sh
@@ -0,0 +1,9 @@
+sudo apt install openjdk-21-jdk -y
+java --version
+sudo groupadd tomcat
+sudo useradd -s /bin/false -g tomcat -d /opt/tomcat tomcat
+wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.117/bin/apache-tomcat-9.0.117.tar.gz
+sudo mkdir /opt/tomcat
+sudo tar -xvzf apache-tomcat-9.0.117.tar.gz -C /opt/tomcat --strip-components=1
+sudo chown -R tomcat:tomcat /opt/tomcat
+sudo chmod g+x /opt/tomcat/conf
