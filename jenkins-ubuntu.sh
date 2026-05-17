52e6f180af62b9c75be8260f6b29672baf9b914c
Aditya-Vardhan-Korutla <150161274+Aditya-Vardhan-Korutla@users.noreply.github.com>
2026-05-17 12:00:14 +0530

    Update Jenkins repository key and source URL

--- jenkins-ubuntu.sh
+++ jenkins-ubuntu.sh
@@ -4,11 +4,11 @@ sudo apt update -y
 sudo apt upgrade -y
 sudo apt install -y openjdk-21-jdk -y
 java -version
-curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
+curl -fsSL https://pkg.origin.jenkins.io/debian-stable/jenkins.io-2026.key | sudo tee \
   /usr/share/keyrings/jenkins-keyring.asc > /dev/null
 
-echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
-  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.origin.jenkins.io/debian-stable/ binary/" | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
 sudo apt update -y
 sudo apt install -y jenkins
